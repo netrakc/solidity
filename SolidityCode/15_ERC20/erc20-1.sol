33747e27505c24e8a421a74e859a89a9ff1055d2
amishalamichhane <amisha.lamichhane1@gmail.com>
2023-02-08 06:17:15 +0545

    erc20-projects with solidity

---
+++ SolidityCode/15_ERC20/erc20-1.sol
@@ -0,0 +1,20 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// contract IneuronToken is ERC20{
+
+//     // constructor will initializes a smart contract
+//     constructor(uint256 initialSupply)  ERC20("Ineuron", "INT"){
+//         // _mint is used to create new tokens
+//         _mint(msg.sender, initialSupply);
+//     }
+
+// }
+
+// 1. check the total supply, name of token, symbol, check the balance of owner address.
+// 2. 
+
+
+
